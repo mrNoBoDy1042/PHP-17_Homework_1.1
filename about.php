dee59e1827d167b4048ac918c65926b52499b6f4
mrNoBoDy <mrnobody1042@ya.ru>
2017-10-22 12:19:44 +0300

    Сделано: создать переменные о себе, и вывести их в шаблоне HTML

---
+++ about.php
@@ -0,0 +1,16 @@
+<meta charset="UTF-8" />
+<?php
+$name = 'Валрий';
+$age = 19;
+$email = 'mrnobody1042@ya.ru';
+$city = 'Москва';
+$about_me = 'Студент, люблю программировать.';
+?>
+<div>
+<H1>Информация обо мне:</H1>
+<p><strong>Имя:</strong> <?= $name?> </p>
+<p><strong>Возраст:</strong> <?= $age?> </p>
+<p><strong>E-mail:</strong> <?= $email?> </p>
+<p><strong>Город:</strong> <?= $city?> </p>
+<p><strong>Обо мне:</strong> <?= $about_me?> </p>
+</div>
